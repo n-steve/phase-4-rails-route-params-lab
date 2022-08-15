5e306c563973036cb834b079faeb870ff99dae56
steve <steve.nguyen2013@gmail.com>
2022-08-15 16:45:30 -0700

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+students = Student.all
+render json: students
   end
+
+def show
+  students = Student.find(params[:id])
+  render json: students
+end
+
 
 end
